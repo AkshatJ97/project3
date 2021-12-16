afcffedd2c73465322627d536330f3d5f6ebda3c
AkshatJ1997 <aj682@njit.edu>
2021-12-16 13:29:29 -0500

    project_3

--- app/controllers/calculator_controller.py
+++ app/controllers/calculator_controller.py
@@ -1,6 +1,7 @@
 from app.controllers.controller import ControllerBase
 from calculator.main import Calculator
 from flask import render_template, request, flash, redirect, url_for
+import pandas as pd
 
 
 class CalculatorController(ControllerBase):
@@ -18,15 +19,25 @@ class CalculatorController(ControllerBase):
             flash('Please enter valid input for Value 1 field')
         elif request.form['value2'] == '':
             flash('Please enter valid input for Value 2 field')
+        elif request.form['value2'] == '0' and operation == 'division':
+            flash('Cannot divide by 0 !!')
         else:
-            flash('CALCULATION SUCCESSFUL')
+            flash(str(operation).capitalize() + ' CALCULATION SUCCESSFUL')
 
             # make the tuple
             my_tuple = (value1, value2)
             # this will call the correct operation
             getattr(Calculator, operation)(my_tuple)
             result = str(Calculator.get_last_result_value())
-            return render_template('result.html', value1=value1, value2=value2, operation=operation, result=result)
+
+            # write to csv
+            calculations = {'Value1': [value1], 'Value2': [value2], 'Operation': [operation], 'Result': [result]}
+            df = pd.DataFrame(calculations, columns=['Value1', 'Value2', 'Operation', 'Result'])
+            df.to_csv('output.csv', mode='a', index=False, header=False, sep=',')
+
+            hist = pd.read_csv('output.csv', skiprows=1).values.tolist()
+
+            return render_template('result.html', val1=value1, val2=value2, ope=operation, res=result, hist=hist)
         return render_template('basicform.html')
 
     @staticmethod
